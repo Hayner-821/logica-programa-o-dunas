64a5dd774304761c851362fb9cd3a7934c8af810
Seu Nome <seu-email@example.com>
2025-03-28 11:24:14 -0300

    primeiro.Progrograma

---
+++ primeiroPrograma.py
@@ -0,0 +1,6 @@
+# criação e declaração de variável
+nome= "hayner"
+print(nome)
+numeroMatricula= 202304171017
+#associação de variavel
+print(f"o teu nome é: {nome} e a tua matricula é: {numeroMatricula}")
