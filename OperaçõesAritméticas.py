d39c99a67213789a448f658d52ff56fa60f8297d
Seu Nome <seu-email@example.com>
2025-04-19 18:57:42 -0300

    ultimas tentativas

--- OperaçõesAritméticas.py
+++ OperaçõesAritméticas.py
@@ -1,6 +1,6 @@
 #Adição
-numero1=10
-numero2=20
+numero1 =10
+numero2 =20
 soma= numero1 + numero2
 print(soma)
 
--- exercicios/exercicio004.py
+++
@@ -1,6 +0,0 @@
-# Exercício 4: Peça a idade do usuário e diga se ele é maior de idade ou não.
-idade=(input(str('Digite sua idade: ') ))
-
-# Transformar a idade em inteiro
-if idade >= 18:
-    print('Maior de idade')
